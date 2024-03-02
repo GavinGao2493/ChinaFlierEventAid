72f81af5a7ef03909070b1b138d2c245a859f271
Gavin Gao <Gavin Gao@DESKTOP-D28OSDQ>
2024-03-02 16:19:15 +0800

    添加项目文件。

---
+++ ConsoleVersion/AircraftListData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+namespace ConsoleVersion
+{
+    /// <summary>
+    /// 存储单个机组信息
+    /// </summary>
+    class Aircraft()
+    {
+        /// <summary>
+        /// 到达机场ICAO码
+        /// </summary>
+        public string AirportA = "";
+        /// <summary>
+        /// 到达机场IATA码
+        /// </summary>
+        public string AirportAIATA = "";
+        /// <summary>
+        /// 到达机场名称
+        /// </summary>
+        public string AirportAName = "";
+        public string AirportD = "";
+        public string Airport = "";
+    }
+    /// <summary>
+    /// 存储全部机组信息（从airline_list中读取）
+    /// </summary>
+    class AircraftList
+    {
+
+    }
+}
---
+++ ConsoleVersion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace ConsoleVersion
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://map.chinaflier.com/airline_list";
+
+            try
+            {
+                string content = await GetWebContent(url);
+
+                Console.WriteLine(content);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"发生错误: {ex.Message}");
+            }
+        }
+        static async Task<string> GetWebContent(string url)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw new Exception($"HTTP 请求失败: {ex.Message}");
+                }
+            }
+        }
+    }
+}
