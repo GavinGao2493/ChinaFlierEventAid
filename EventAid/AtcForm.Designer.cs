28d317569072dc733d1a825967ffee9d341ab992
Gavin Gao <gaoyanghao0311@163.com>
2024-03-06 13:31:06 +0800

    增加了AtcForm的大量元件，以及与之配套的相关代码

--- EventAid/AtcForm.Designer.cs
+++ EventAid/AtcForm.Designer.cs
@@ -28,7 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
+            labelDep = new Label();
+            listBox1 = new ListBox();
+            labelDepIcao = new Label();
+            textBoxDepIcao = new TextBox();
+            buttonDepIcao = new Button();
+            labelHelper1 = new Label();
+            labelArr = new Label();
+            labelHelper2 = new Label();
+            labelArrIcao = new Label();
+            textBoxArrIcao = new TextBox();
+            buttonArrIcao = new Button();
+            listBox2 = new ListBox();
+            buttonExport = new Button();
             SuspendLayout();
+            // 
+            // labelDep
+            // 
+            labelDep.AutoSize = true;
+            labelDep.Location = new Point(44, 55);
+            labelDep.Name = "labelDep";
+            labelDep.Size = new Size(69, 20);
+            labelDep.TabIndex = 0;
+            labelDep.Text = "起飞机场";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(142, 55);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(119, 304);
+            listBox1.TabIndex = 1;
+            listBox1.MouseDoubleClick += listBox1_MouseDoubleClick;
+            listBox1.MouseUp += listBox1_MouseUp;
+            // 
+            // labelDepIcao
+            // 
+            labelDepIcao.AutoSize = true;
+            labelDepIcao.Location = new Point(44, 253);
+            labelDepIcao.Name = "labelDepIcao";
+            labelDepIcao.Size = new Size(76, 20);
+            labelDepIcao.TabIndex = 2;
+            labelDepIcao.Text = "机场ICAO";
+            // 
+            // textBoxDepIcao
+            // 
+            textBoxDepIcao.Location = new Point(41, 287);
+            textBoxDepIcao.MaxLength = 4;
+            textBoxDepIcao.Name = "textBoxDepIcao";
+            textBoxDepIcao.Size = new Size(82, 27);
+            textBoxDepIcao.TabIndex = 3;
+            // 
+            // buttonDepIcao
+            // 
+            buttonDepIcao.Location = new Point(41, 330);
+            buttonDepIcao.Name = "buttonDepIcao";
+            buttonDepIcao.Size = new Size(82, 29);
+            buttonDepIcao.TabIndex = 4;
+            buttonDepIcao.Text = "添加机场";
+            buttonDepIcao.UseVisualStyleBackColor = true;
+            buttonDepIcao.Click += buttonDepIcao_Click;
+            // 
+            // labelHelper1
+            // 
+            labelHelper1.AutoSize = true;
+            labelHelper1.Location = new Point(37, 148);
+            labelHelper1.Name = "labelHelper1";
+            labelHelper1.Size = new Size(99, 40);
+            labelHelper1.TabIndex = 5;
+            labelHelper1.Text = "双击机场名称\r\n即可删除项目";
+            // 
+            // labelArr
+            // 
+            labelArr.AutoSize = true;
+            labelArr.Location = new Point(349, 55);
+            labelArr.Name = "labelArr";
+            labelArr.Size = new Size(69, 20);
+            labelArr.TabIndex = 6;
+            labelArr.Text = "落地机场";
+            // 
+            // labelHelper2
+            // 
+            labelHelper2.AutoSize = true;
+            labelHelper2.Location = new Point(332, 148);
+            labelHelper2.Name = "labelHelper2";
+            labelHelper2.Size = new Size(99, 40);
+            labelHelper2.TabIndex = 7;
+            labelHelper2.Text = "双击机场名称\r\n即可删除项目";
+            // 
+            // labelArrIcao
+            // 
+            labelArrIcao.AutoSize = true;
+            labelArrIcao.Location = new Point(340, 253);
+            labelArrIcao.Name = "labelArrIcao";
+            labelArrIcao.Size = new Size(76, 20);
+            labelArrIcao.TabIndex = 8;
+            labelArrIcao.Text = "机场ICAO";
+            // 
+            // textBoxArrIcao
+            // 
+            textBoxArrIcao.Location = new Point(336, 287);
+            textBoxArrIcao.MaxLength = 4;
+            textBoxArrIcao.Name = "textBoxArrIcao";
+            textBoxArrIcao.Size = new Size(82, 27);
+            textBoxArrIcao.TabIndex = 9;
+            // 
+            // buttonArrIcao
+            // 
+            buttonArrIcao.Location = new Point(336, 330);
+            buttonArrIcao.Name = "buttonArrIcao";
+            buttonArrIcao.Size = new Size(82, 29);
+            buttonArrIcao.TabIndex = 10;
+            buttonArrIcao.Text = "添加机场";
+            buttonArrIcao.UseVisualStyleBackColor = true;
+            buttonArrIcao.Click += buttonArrIcao_Click;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(441, 55);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(119, 304);
+            listBox2.TabIndex = 11;
+            listBox2.MouseDoubleClick += listBox2_MouseDoubleClick;
+            listBox2.MouseUp += listBox2_MouseUp;
+            // 
+            // buttonExport
+            // 
+            buttonExport.Location = new Point(232, 413);
+            buttonExport.Name = "buttonExport";
+            buttonExport.Size = new Size(129, 53);
+            buttonExport.TabIndex = 12;
+            buttonExport.Text = "导出当前时刻的管制列表";
+            buttonExport.UseVisualStyleBackColor = true;
             // 
             // AtcForm
             // 
@@ -37,14 +169,43 @@
             BackColor = SystemColors.ControlLight;
             ClientSize = new Size(600, 550);
             ControlBox = false;
+            Controls.Add(buttonExport);
+            Controls.Add(listBox2);
+            Controls.Add(buttonArrIcao);
+            Controls.Add(textBoxArrIcao);
+            Controls.Add(labelArrIcao);
+            Controls.Add(labelHelper2);
+            Controls.Add(labelArr);
+            Controls.Add(labelHelper1);
+            Controls.Add(buttonDepIcao);
+            Controls.Add(textBoxDepIcao);
+            Controls.Add(labelDepIcao);
+            Controls.Add(listBox1);
+            Controls.Add(labelDep);
             ForeColor = SystemColors.ControlText;
             FormBorderStyle = FormBorderStyle.None;
             Name = "AtcForm";
             ShowIcon = false;
             Text = "Form1";
+            Load += AtcForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labelDep;
+        private ListBox listBox1;
+        private Label labelDepIcao;
+        private TextBox textBoxDepIcao;
+        private Button buttonDepIcao;
+        private Label labelHelper1;
+        private Label labelArr;
+        private Label labelHelper2;
+        private Label labelArrIcao;
+        private TextBox textBoxArrIcao;
+        private Button buttonArrIcao;
+        private ListBox listBox2;
+        private Button buttonExport;
     }
 }
--- EventAid/AtcForm.cs
+++ EventAid/AtcForm.cs
@@ -7,14 +7,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace EventAidForm
 {
     public partial class AtcForm : Form
     {
+        static bool IsEnglish(string input)
+        {
+            // 使用正则表达式匹配字符串是否只包含英文字符
+            Regex regex = new Regex("^[a-zA-Z]+$");
+            return regex.IsMatch(input);
+        }
+
         public AtcForm()
         {
             InitializeComponent();
+        }
+
+        private void AtcForm_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void buttonDepIcao_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxDepIcao.Text) || !IsEnglish(textBoxDepIcao.Text))
+            {
+                MessageBox.Show("请输入合法的机场ICAO码");
+                return;
+            }
+            textBoxDepIcao.Text = textBoxDepIcao.Text.ToUpper();
+            if (listBox1.Items.Contains(textBoxDepIcao.Text))
+            {
+                MessageBox.Show("机场已存在！");
+                return;
+            }
+            listBox1.Items.Add(textBoxDepIcao.Text);
+            textBoxDepIcao.Text = string.Empty;
+        }
+
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // 取消选中项的选择
+            listBox1.ClearSelected();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // 获取双击的项的索引
+            int index = listBox1.IndexFromPoint(e.Location);
+
+            // 如果双击发生在有效的项上
+            if (index != ListBox.NoMatches)
+            {
+                // 删除双击的项
+                listBox1.Items.RemoveAt(index);
+            }
+        }
+
+        private void buttonArrIcao_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxArrIcao.Text) || !IsEnglish(textBoxArrIcao.Text))
+            {
+                MessageBox.Show("请输入合法的机场ICAO码");
+                return;
+            }
+            textBoxArrIcao.Text = textBoxArrIcao.Text.ToUpper();
+            if (listBox2.Items.Contains(textBoxArrIcao.Text))
+            {
+                MessageBox.Show("机场已存在！");
+                return;
+            }
+            listBox2.Items.Add(textBoxArrIcao.Text);
+            textBoxArrIcao.Text = string.Empty;
+        }
+
+        private void listBox2_MouseUp(object sender, MouseEventArgs e)
+        {
+            // 取消选中项的选择
+            listBox2.ClearSelected();
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // 获取双击的项的索引
+            int index = listBox2.IndexFromPoint(e.Location);
+
+            // 如果双击发生在有效的项上
+            if (index != ListBox.NoMatches)
+            {
+                // 删除双击的项
+                listBox2.Items.RemoveAt(index);
+            }
         }
     }
 }
