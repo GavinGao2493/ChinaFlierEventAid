497767a7429cb785eee2b1f11e99de6410a4c9ee
Gavin Gao <gaoyanghao0311@163.com>
2024-03-06 10:04:05 +0800

    新建了一个窗口

---
+++ EventAid/AircraftForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace EventAidForm
+{
+    partial class AircraftForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // AircraftForm
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(600, 550);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "AircraftForm";
+            Text = "AircraftForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
